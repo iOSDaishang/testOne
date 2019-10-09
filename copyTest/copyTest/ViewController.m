98049ed29052f72f68f5f38c98cc0238f0eab764
daishang <daishang@niwodai.net>
2019-10-09 15:53:17 +0800

    测试4

--- copyTest/copyTest/ViewController.m
+++ copyTest/copyTest/ViewController.m
@@ -64,6 +64,12 @@
 
     NSLog(@"test123123");
     NSLog(@"test123123");
+    NSLog(@"test123123");
+    NSLog(@"test123123");
+    NSLog(@"test123123");
+    NSLog(@"test123123");
+    NSLog(@"test123123");
+    NSLog(@"test123123");
     
 //    [XianliaoApiManager registerApp:@"yapSYw221SGViW7N"];
     
